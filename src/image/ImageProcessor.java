06c4d66be7e1b720b0e501df3f4498e9e81d8344
Owner-Hadas <hadas.elezra@mail.huji.ac.il>
2024-12-22 10:52:04 +0200

    creating class ImageProcessor split image

--- src/image/ImageProcessor.java
+++ src/image/ImageProcessor.java
@@ -18,8 +18,8 @@ public class ImageProcessor {
      * Pads the given image to the nearest power of 2 dimensions and centers it.
      *
      * @param pixelArray The pixel array of the original image.
-     * @param width The width of the original image.
-     * @param height The height of the original image.
+     * @param width      The width of the original image.
+     * @param height     The height of the original image.
      * @return A new Image object representing the padded image with the original centered.
      */
     public static Image padImageToPowerOfTwo(Color[][] pixelArray, int width, int height) {
@@ -38,7 +38,8 @@ public class ImageProcessor {
         for (int r = 0; r < paddedHeight; r++) {
             for (int c = 0; c < paddedWidth; c++) {
                 paddedPixelArray[startRow + r][startCol + c] = new Color(pixelArray[r][c].getRed(),
-                        pixelArray[r][c].getGreen(), pixelArray[r][c].getBlue()); // todo validate creating obj
+                        pixelArray[r][c].getGreen(), pixelArray[r][c].getBlue()); // todo validate creating
+                // obj
             }
         }
 
@@ -57,6 +58,92 @@ public class ImageProcessor {
         }
         // Return a new Image object with the padded pixel array
         return new Image(paddedPixelArray, paddedWidth, paddedHeight);
+    }
+
+    /**
+     * Divides the given padded image into sub-images based on the resolution.
+     *
+     * @param paddedImage   The padded image to divide.
+     * @param numCharsInRow The number of characters in each row of the ASCII art.
+     * @return A 2D array of sub-images representing the divided image.
+     */
+    public static Image[][] divideImageIntoSubImages(Image paddedImage, int numCharsInRow) {
+        int imageWidth = paddedImage.getWidth();
+        int imageHeight = paddedImage.getHeight();
+
+        // Calculate sub-image dimensions
+        int squareSize = imageWidth / numCharsInRow;
+
+        // Calculate the number of rows of square sub-images
+        int numRows = imageHeight / squareSize;
+
+        // Create a 2D array to hold the sub-images
+        Image[][] subImages = new Image[numRows][numCharsInRow];
+
+        // Divide the image into sub-images
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCharsInRow; col++) {
+                int startRow = row * squareSize;
+                int endRow = startRow + squareSize;
+                int startCol = col * squareSize;
+                int endCol = startCol + squareSize;
+
+                // Extract the sub-image
+                Image subImage = extractSubImage(paddedImage,
+                        startRow, endRow, startCol, endCol);
+
+                // Store the sub-image in the array
+                subImages[row][col] = subImage;
+            }
+        }
+        return subImages;
+    }
+
+    /**
+     * Calculates the brightness of image
+     *
+     * @param image The 2D array of square sub-images.
+     * @return A 2D array of brightness values corresponding to each sub-image.
+     */
+    public static double calculateBrightness(Image image) {
+        int numRows = image.getWidth();
+        int numCols = image.getHeight();
+
+        double sumBrightness = 0;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                Color pixelColor = image.getPixel(row, col);
+                double greyPixel =
+                        pixelColor.getRed() * 0.2126 + pixelColor.getGreen() * 0.7152 + pixelColor.getBlue() * 0.0722;
+                sumBrightness += greyPixel;
+            }
+        }
+        return sumBrightness / (numRows * numCols);
+    }
+
+
+    /**
+     * Extracts a sub-image from the given image based on the specified bounds.
+     *
+     * @param image    The original image.
+     * @param startRow The starting row index.
+     * @param endRow   The ending row index.
+     * @param startCol The starting column index.
+     * @param endCol   The ending column index.
+     * @return A new Image object representing the sub-image.
+     */
+    private static Image extractSubImage(Image image, int startRow, int endRow, int startCol, int endCol) {
+        int squareSize = endRow - startRow; // גודל הריבוע
+        Color[][] subImagePixels = new Color[squareSize][squareSize];
+
+        for (int r = 0; r < squareSize; r++) {
+            for (int c = 0; c < squareSize; c++) {
+                // Use getPixel to access the original image's pixels
+                subImagePixels[r][c] = image.getPixel(startRow + r, startCol + c);
+            }
+        }
+        return new Image(subImagePixels, squareSize, squareSize);
     }
 
     /**
